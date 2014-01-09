5e2eb4e033cb1a14e9019a349d266488982196be
JFelsinger <JFFelsinger@gmail.com>
2014-01-08 20:31:34 -0500

    More server functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,22 +14,52 @@ module.exports = function(grunt) {
         },
 
         watch: {
+            options: {
+                spawn: false
+            },
+
             css: {
                 files: 'assets/css/_sass/**',
-                tasks: ['sass'],
-                options: {
-                    spawn: false
-                }
+                tasks: ['sass']
+            },
+
+            jekyll: {
+                files: './**',
+                tasks: ['jekyll:build']
             }
         },
 
         jekyll: {
+            options: {
+                config: '_config.yml'
+            },
+
+            serve: {
+                options: {
+                    serve: true
+                }
+            },
+
+            build: {
+            }
+        },
+
+        concurrent: {
+            server: {
+                tasks: ['watch', 'jekyll:serve'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-jekyll');
 
-    grunt.registerTask('default', ['sass', 'watch']);
+    grunt.registerTask('default', ['sass', 'jekyll:build']);
+
+    grunt.registerTask('server', ['sass', 'concurrent:server']);
 }
